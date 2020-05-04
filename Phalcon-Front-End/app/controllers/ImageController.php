61dac1410397424cb3879ae51a9985da9c1a8fef
ranggakd <ranggakusumad@gmail.com>
2020-05-05 06:38:57 +0700

    image done

--- Phalcon-Front-End/app/controllers/ImageController.php
+++ Phalcon-Front-End/app/controllers/ImageController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use Phalcon\Image\Adapter\Gd as Gd;
+use Phalcon\Image\ImageFactory;
+use Phalcon\Image\Adapter\Gd;
+use Phalcon\Image\Exception;
 use Phalcon\Image\Enum;
 
 class ImageController extends ControllerBase
@@ -11,22 +13,147 @@ class ImageController extends ControllerBase
 
     public function indexAction()
     {
-        // getter
-        $image = new Gd('/images/jon-bellion-front-cover-the_human_condition.jpg');
-        // $height = $image->getHeight();          // int
-        // // $image = $image->getImage();            // mixed
-        // $mime = $image->getMime();              // str
-        // $realpath = $image->getRealpath();      // str
-        // $type = $image->getType();              // int
-        // $width = $image->getWidth();            // int
-        // $this->view->setVars([
-        //     'height' => $height,
-        //     // 'image' => $image,
-        //     'mime' => $mime,
-        //     'realpath' => $realpath,
-        //     'type' => $type,
-        //     'width' => $width
-        // ]);
+        // getter from image file on /public/images/
+        $dir = 'images/';
+        $filepath = 'jon-bellion-front-cover-the_human_condition.jpg';
+        $image = new Gd($dir.$filepath);
+        $height = $image->getHeight();          // int
+        $images = $image->getImage();           // mixed
+        $mime = $image->getMime();              // str
+        $realpath = $image->getRealpath();      // str
+        $type = $image->getType();              // int
+        $width = $image->getWidth();            // int
+        $this->view->setVars([
+            'height' => $height,
+            'images' => $images,
+            'mime' => $mime,
+            'realpath' => $realpath,
+            'type' => $type,
+            'width' => $width,
+            'filepath' => $dir.$filepath
+        ]);
+
+        // blur, radius int
+        $image = new Gd($dir.$filepath);
+        $image->blur(70);
+        $image->save($dir."blur_".$filepath);
+        $this->view->setVar('blur',$dir."blur_".$filepath);
+
+        // crop, width int, height int, offsetX int, offsetT int
+        $image = new Gd($dir.$filepath);
+        $w = 150;
+        $h = $w;
+        $offsetX = ($width-$w)/2;
+        $offsetY = ($height-$h)/2;
+        $image->crop($w, $h, $offsetX, $offsetY);
+        $image->save($dir."crop_".$filepath);
+        $this->view->setVar('crop',$dir."crop_".$filepath);    
+        
+        // flip, const ENUM
+        $image = new Gd($dir.$filepath);
+        $image->flip(Enum::HORIZONTAL);
+        $image->save($dir."flip_".$filepath);
+        $this->view->setVar('flip',$dir."flip_".$filepath);
+
+        // pixelate, pixel int
+        $image = new Gd($dir.$filepath);
+        $image->pixelate(20);
+        $image->save($dir."pixel_".$filepath);
+        $this->view->setVar('pixel',$dir."pixel_".$filepath);
+
+        // reflection, height int, opacity int, fadeIn bool
+        $image = new Gd($dir.$filepath);
+        $image->reflection(100,75,true);
+        $image->save($dir."reflection_".$filepath);
+        $this->view->setVar('reflection',$dir."reflection_".$filepath);
+
+        // render, ext str, quality int
+        $image = new Gd($dir.$filepath);
+        $render = $image->render('jpg',90);
+        $this->view->setVar('render',$render);
+
+        // resize, width int, height int, master int (const ENUM) default auto
+        // const HEIGHT
+        $image = new Gd($dir.$filepath);
+        $image->resize(null,200,Enum::HEIGHT);
+        $image->save($dir."resize_h_".$filepath);
+        $this->view->setVar('resize_h',$dir."resize_h_".$filepath);        
+
+        // const WIDTH
+        $image = new Gd($dir.$filepath);
+        $image->resize(150,null,Enum::WIDTH);
+        $image->save($dir."resize_w_".$filepath);
+        $this->view->setVar('resize_w',$dir."resize_w_".$filepath);
+
+        // rotasi, degree int
+        $image = new Gd($dir.$filepath);
+        $image->rotate(90);
+        // save, filename str, quality int
+        $image->save($dir."rotate_".$filepath, 90);
+        $this->view->setVar('rotate',$dir."rotate_".$filepath);                
+
+        // sharpen, radius int
+        $image = new Gd($dir.$filepath);
+        $image->sharpen(70);
+        $image->save($dir."sharpen_".$filepath);
+        $this->view->setVar('sharpen',$dir."sharpen_".$filepath);
+
+        // text, text str, offsetX int, offsetY int, opacity int
+        // color str, size int, fontfile str
+        $image = new Gd($dir.$filepath);
+        $image->text(
+            'Human Condition',
+            80,                // false to disable
+            150,                // false to disable
+            65,
+            'ffffff',           // hexdec   
+            20,
+            null
+        );
+        $image->save($dir."text_".$filepath);
+        $this->view->setVar('text',$dir."text_".$filepath);            
+
+        // watermark, watermark obj Gd, offsetX int, offsetY int, opacity int
+        $image = new Gd($dir.$filepath);
+        $watermark = new Gd($dir."twitter.png");
+        $watermark->resize(100,100);
+        $offsetX = ($width-$watermark->getWidth()-10);
+        $offsetY = ($height-$watermark->getHeight()-10);
+        $image->watermark(
+            $watermark,
+            $offsetX,
+            $offsetY,
+            60
+        );
+        $image->save($dir."watermark_".$filepath);
+        $this->view->setVar('watermark',$dir."watermark_".$filepath); 
+        
+        // factory
+        // new instance, name str, file str, width int, height int
+        $factory = new ImageFactory();
+        $image = $factory->newInstance('gd', $dir.$filepath);
+        $this->view->setVar('factory',$image->getRealpath()); 
+
+        // load, adapter ai, file str, width int, height int
+        $factory = new ImageFactory();
+        $opt = [
+            'adapter' => 'gd',
+            'file' => $dir.$filepath,
+            'width' => 200,
+            'height' => 200,
+        ];
+        $image = $factory->load($opt);
+        $this->view->setVars([
+            'w_load' => $image->getWidth(),
+            'h_load' => $image->getHeight()
+        ]); 
+
+        // exception
+        try {
+            $image = new Gd('salah_filepath');
+        } catch (Exception $ex) {
+            $this->flashSession->error($ex->getMessage());
+        }
     }
 }
 
--- Phalcon-Front-End/app/views/image/index.volt
+++ Phalcon-Front-End/app/views/image/index.volt
@@ -10,7 +10,88 @@
     </head>
     <body>
         <div class="container">
-            <img src="/images/jon-bellion-front-cover-the_human_condition.jpg">
+            <div class="page-header">
+                <h3>Getter from Gd</h3>
+                <img src="{{filepath}}">
+                <p>height: {{height}}</p>
+                <p>image: {{images}}</p>
+                <p>mime: {{mime}}</p>
+                <p>realpath: {{realpath}}</p>
+                <p>type: {{type}}</p>
+                <p>width: {{width}}</p>
+            </div>
+            <div class="page-header">
+                <h3>blur</h3>
+                <img src="{{filepath}}">
+                <img src="{{blur}}">
+            </div>            
+            <div class="page-header">
+                <h3>crop</h3>
+                <img src="{{filepath}}">
+                <img src="{{crop}}">
+            </div>            
+            <div class="page-header">
+                <h3>flip</h3>
+                <img src="{{filepath}}">
+                <img src="{{flip}}">
+            </div>            
+            <div class="page-header">
+                <h3>pixelate</h3>
+                <img src="{{filepath}}">
+                <img src="{{pixel}}">
+            </div>
+            <div class="page-header">
+                <h3>reflection</h3>
+                <img src="{{filepath}}">
+                <img src="{{reflection}}">
+            </div>        
+            <div class="page-header">
+                <h3>render</h3>
+                <img src="{{filepath}}">
+                {% if render %}
+                <p>telah di render</p>
+                {% endif %}
+            </div>
+            <div class="page-header">
+                <h3>resize height</h3>
+                <img src="{{filepath}}">
+                <img src="{{resize_h}}">
+            </div>        
+            <div class="page-header">
+                <h3>resize width</h3>
+                <img src="{{filepath}}">
+                <img src="{{resize_w}}">
+            </div>                                
+            <div class="page-header">
+                <h3>rotasi</h3>
+                <img src="{{filepath}}">
+                <img src="{{rotate}}">
+            </div>                    
+            <div class="page-header">
+                <h3>sharpen</h3>
+                <img src="{{filepath}}">
+                <img src="{{sharpen}}">
+            </div>        
+            <div class="page-header">
+                <h3>text</h3>
+                <img src="{{filepath}}">
+                <img src="{{text}}">
+            </div>              
+            <div class="page-header">
+                <h3>watermark</h3>
+                <img src="{{filepath}}">
+                <img src="{{watermark}}">
+            </div>                          
+            <div class="page-header">
+                <h3>factory</h3>
+                <p>factory: {{factory}}</p>
+            </div>            
+            <div class="page-header">
+                <h3>load</h3>
+                <p>height: {{h_load}}</p>
+                <p>width: {{w_load}}</p>
+            </div>            
+            {{ flashSession.output() }}
         </div>
     </body>
 </html>
