c107de84fa64a6c42bab32e72db2762551dc9032
Octavianus Giovanni Y <octavianusgiovanni@gmail.com>
2020-05-04 15:26:00 +0700

    edit first page

--- Phalcon-Front-End/cache/_home_ivan_documents_github_phalcon-front-end_phalcon-front-end_app_views_index_index.volt.php
+++ Phalcon-Front-End/cache/_home_ivan_documents_github_phalcon-front-end_phalcon-front-end_app_views_index_index.volt.php
@@ -15,13 +15,11 @@
             
 
     <div class="page-header">
-        <h1>Congratulations!</h1>
+        <h1>Phalcon Front-End</h1>
     </div>
     
-    <p>You're now flying with Phalcon. Great things are about to happen!</p>
+    <p>Assets Management, Flashing Messages, Images</p>
     
-    <p>This page is located at <code>views/index/index.phtml</code></p>
-
 
         </div>
         <!-- jQuery first, then Popper.js, and then Bootstrap's JavaScript -->
